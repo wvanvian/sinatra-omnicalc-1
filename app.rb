5130e78fbe5ed527d6c90274aef88f076417af27
wvanvian <146589485+wvanvian@users.noreply.github.com>
2023-10-21 15:45:17 +0000

    square root and square done

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:layout)
+end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square/results") do
+  @num = params.fetch("number").to_f
+  @square = @num ** 2
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+
+  @num = params.fetch("user_number").to_f
+  @root = Math.sqrt(@num)
+
+  erb(:square_root_results)
+
 end
